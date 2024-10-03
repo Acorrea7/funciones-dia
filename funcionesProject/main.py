72b50006e399ef21ca0bcefe8fbe6a6a5c3ea776
Andres Correa <andrescorrea0599@gmail.com>
2024-10-03 14:52:15 -0500

    Se crea el proyecto con el archivo main y una funcion con el hola mundo

---
+++ funcionesProject/main.py
@@ -0,0 +1,16 @@
+#Definicion de una funcion
+
+def my_funcion():
+    print(" Hello from a function")
+
+print( " es ho es hoy EL QUIZ")
+
+#invocar la funcion
+
+my_funcion()
+
+def mostrarNombre(nombre):
+    print(" Su nombre es " + nombre)
+
+nom = input( "ingrese su nombre: ")
+mostrarNombre(nom)
